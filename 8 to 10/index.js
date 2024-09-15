7def45b2f74bcc5a5a7834075248c9e42af4263a
Muhammad Mubashir <mubashirbehlum606@gmail.com>
2024-09-15 19:08:29 +0500

    Add files via upload

---
+++ 8 to 10/index.js
@@ -0,0 +1,46 @@
+// Chapter 8 to 10
+
+// Q.1
+// var input = prompt("What's Your Name?");
+// console.log(input);
+
+// if (input){
+//     alert("Hello🤝, " + input);
+// }
+
+// Q.2
+// var num = prompt("Please enter a number:");
+//     var tableDiv = document.getElementById("table");
+//     var tableHTML = "";
+
+//     if (num === null || num.trim() === "") {
+//         num = 5;
+//     } else {
+//         num = parseInt(num);}
+//     for (var i = 1; i <= 10; i++) {
+//         tableHTML += "<tr><td>" + num + " x " + i + "
+// " + "</td><td>" + num * i + "</td></tr>";
+//     }
+//     tableDiv.innerHTML = tableHTML;
+
+// Q.3
+//  var city = prompt("Please enter your city name:");
+
+//  if (city.toLowerCase() === "karachi") {
+//      alert("Welcome to the city of lights!");
+//  } else {
+//      alert("Welcome to " + city + "!");
+//  }
+
+// Q.4
+// USER INPUT & CONDITIONAL STATEMENT
+// var gender = prompt("Please enter your gender:");
+
+// if (gender.toLowerCase() === "male") {
+//   alert("Good Morning Sir!");
+// } else if (gender.toLowerCase() === "female") {
+//   alert("Good Morning Ma'am");
+// } else {
+//   alert("Goof Morning " + gender + "!");
+// }
+//! this term "tolowerCase" recommended by Chatgpt
